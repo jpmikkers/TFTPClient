4276181da3a86d724a3e1563b7334b77c6ae8f1f
Jean-Paul Mikkers <jean.paul.mikkers@gmail.com>
2013-04-01 13:23:34 +0000

    Improve command line help text, add silent option

--- TFTPClient/Client/Program.cs
+++ TFTPClient/Client/Program.cs
@@ -27,6 +27,8 @@ using System.Net;
 using System.Net.Sockets;
 using CodePlex.JPMikkers.TFTP.Client;
 using Mono.Options;
+using System.IO;
+using System.Reflection;
 
 namespace Client
 {
@@ -44,109 +46,131 @@ namespace Client
 
         static int Main(string[] args)
         {
-            string localFilename = null;
-            string remoteFilename = null;
-            bool isGet = false;
-            bool isPut = false;
-            bool ipv6 = false;
-            int serverPort = 69;
-            bool showHelp = false;
-            IPAddress serverAddress = null;
-
-            var settings = new TFTPClient.Settings()
-            {
-                ProgressInterval = TimeSpan.FromMilliseconds(200.0),
-                OnProgress = Program.OnProgress
-            };
-
-            var optionSet = new OptionSet 
-            {
-                "Usage: TFTPClient [options]+ host[:port]",
-                "",
-                { "get", "get a file from remote to local", x => { isGet=true; } },
-                { "put", "put a file from local to remote", x => { isPut=true; } },
-                { "local=", "local filename", name => localFilename = name },
-                { "remote=", "remote filename", name => remoteFilename = name },
-                { "p|serverport=", "override server port (default: 69)", (int port) => serverPort = port },
-                { "b|blocksize=", "set blocksize (default: 512)", (int blocksize) => settings.BlockSize = blocksize },
-                { "t|timeout=", "set response timeout [s] (default: 2)", (int timeout) => settings.ResponseTimeout = TimeSpan.FromSeconds(timeout) },
-                { "r|retries=", "set maximum retries (default: 5)", (int retries) => settings.Retries = retries },
-                { "v|verbose", "generate verbose tracing", x => { settings.OnTrace = Program.OnTrace; } },
-                { "6|ipv6", "resolve hostname to an ipv6 address", x => { ipv6=true; } },
-                { "dontfragment", "don't allow packet fragmentation (default: allowed)", x => settings.DontFragment = (x!=null) },
-                { "ttl=", "set time to live", (short ttl) => settings.Ttl = ttl },
-                { "?|h|help", "show help", x => { showHelp=true; } },
-            };
-
-            var remaining=optionSet.Parse(args);
-
-            if (showHelp)
-            {
-                Console.WriteLine("Help:");
-                optionSet.WriteOptionDescriptions(Console.Out);
-                return 0;
-            }
-
-            if (!isGet && !isPut)
-            {
-                Console.Error.WriteLine("You have to specify /get or /put");
-                return 1;
-            }
-
-            if (localFilename == null || remoteFilename == null)
-            {
-                Console.Error.WriteLine("/local and /remote are mandatory");
-                return 1;
-            }
-
-            if (remaining.Count < 1)
-            {
-                Console.Error.WriteLine("You have to specify the server host name or ip address");
-                return 1;
-            }
-
-            if (IPAddress.TryParse(remaining[0], out serverAddress))
-            {
-                // now try to find whether a port was specified
-                var components = remaining[0].Split(new string[] { serverAddress.AddressFamily == AddressFamily.InterNetworkV6 ? "]:" : ":" }, StringSplitOptions.RemoveEmptyEntries);
-                if (components.Length > 1) int.TryParse(components[1], out serverPort);
-            }
-            else
-            {
-                var components = remaining[0].Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
-                serverAddress = Dns.GetHostEntry(components[0]).AddressList.Where(x => x.AddressFamily == (ipv6 ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork)).FirstOrDefault();
-                if (components.Length > 1) int.TryParse(components[1], out serverPort);
-            }
-
-            if (serverAddress == null)
-            {
-                Console.Error.WriteLine("Could not resolve '{0}' to an {1} address", remaining[0], ipv6 ? "ipv6" : "ipv4");
-                return 1;
-            }
-
             try
             {
-                if (isPut)
+                string localFilename = null;
+                string remoteFilename = null;
+                bool isGet = false;
+                bool isPut = false;
+                bool ipv6 = false;
+                int serverPort = 69;
+                bool showHelp = false;
+                bool silent = false;
+                IPAddress serverAddress = null;
+
+                var settings = new TFTPClient.Settings()
                 {
-                    TFTPClient.Upload(
-                        new IPEndPoint(serverAddress, serverPort),
-                        localFilename,
-                        remoteFilename,
-                        settings);
+                    ProgressInterval = TimeSpan.FromMilliseconds(200.0),
+                    OnProgress = Program.OnProgress
+                };
+
+                var optionSet = new OptionSet 
+                {
+                    "",
+                    string.Format("TFTPClient {0}",Assembly.GetExecutingAssembly().GetName().Version.ToString(3)),
+                    "Transfers files to and from a remote computer running the TFTP service.",
+                    "",
+                    "Usage: TFTPClient [options]+ host[:port]",
+                    "",
+                    { "get", "get a file from remote to local", x => { isGet=true; } },
+                    { "put", "put a file from local to remote", x => { isPut=true; } },
+                    { "local=", "local filename", name => localFilename = name },
+                    { "remote=", "remote filename", name => remoteFilename = name },
+                    { "serverport=", "override server port (default: 69)", (int port) => serverPort = port },
+                    { "blocksize=", "set blocksize (default: 512)", (int blocksize) => settings.BlockSize = blocksize },
+                    { "timeout=", "set response timeout [s] (default: 2)", (int timeout) => settings.ResponseTimeout = TimeSpan.FromSeconds(timeout) },
+                    { "retries=", "set maximum retries (default: 5)", (int retries) => settings.Retries = retries },
+                    { "verbose", "generate verbose tracing", x => { settings.OnTrace = Program.OnTrace; } },
+                    { "ipv6", "resolve hostname to an ipv6 address", x => { ipv6=true; } },
+                    { "dontfragment", "don't allow packet fragmentation (default: allowed)", x => settings.DontFragment = (x!=null) },
+                    { "silent", "don't show progress information", x => silent = (x!=null) },
+                    { "ttl=", "set time to live", (short ttl) => settings.Ttl = ttl },
+                    { "?|h|help", "show help", x => { showHelp=true; } },
+                    "",
+                    "You may use -, --, or / as option delimiters",
+                    "",
+                    "'host' may be specified as a hostname, ipv4 address or ipv6 address.",
+                    "To specify the port number for a ipv6 address you should put the address",
+                    "in square brackets, e.g. [::1]:69",
+                    "",
+                    "Example: (downloading a file 'image.bin' from a server at 192.168.1.23)",
+                    "\tTFTPClient /get /local=image.bin /remote=image.bin 192.168.1.23",
+                };
+
+                var remaining = optionSet.Parse(args);
+
+                if (showHelp || remaining.Count < 1)
+                {
+                    optionSet.WriteOptionDescriptions(Console.Out);
+                    return 0;
+                }
+
+                if (silent)
+                {
+                    settings.OnProgress = null;
+                }
+
+                if (!isGet && !isPut)
+                {
+                    Console.Error.WriteLine("You have to specify /get or /put");
+                    return 1;
+                }
+
+                if (localFilename == null || remoteFilename == null)
+                {
+                    Console.Error.WriteLine("/local and /remote are mandatory");
+                    return 1;
+                }
+
+                if (IPAddress.TryParse(remaining[0], out serverAddress))
+                {
+                    // now try to find whether a port was specified
+                    var components = remaining[0].Split(new string[] { serverAddress.AddressFamily == AddressFamily.InterNetworkV6 ? "]:" : ":" }, StringSplitOptions.RemoveEmptyEntries);
+                    if (components.Length > 1) int.TryParse(components[1], out serverPort);
                 }
                 else
                 {
-                    TFTPClient.Download(
-                        new IPEndPoint(serverAddress, serverPort),
-                        localFilename,
-                        remoteFilename,
-                        settings);
+                    var components = remaining[0].Split(new char[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+                    serverAddress = Dns.GetHostEntry(components[0]).AddressList.Where(x => x.AddressFamily == (ipv6 ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork)).FirstOrDefault();
+                    if (components.Length > 1) int.TryParse(components[1], out serverPort);
                 }
-                return 0;
+
+                if (serverAddress == null)
+                {
+                    Console.Error.WriteLine("Could not resolve '{0}' to an {1} address", remaining[0], ipv6 ? "ipv6" : "ipv4");
+                    return 1;
+                }
+
+                try
+                {
+                    if (isPut)
+                    {
+                        TFTPClient.Upload(
+                            new IPEndPoint(serverAddress, serverPort),
+                            localFilename,
+                            remoteFilename,
+                            settings);
+                    }
+                    else
+                    {
+                        TFTPClient.Download(
+                            new IPEndPoint(serverAddress, serverPort),
+                            localFilename,
+                            remoteFilename,
+                            settings);
+                    }
+                    if (!silent) Console.WriteLine("Transfer complete.");
+                    return 0;
+                }
+                catch (Exception e)
+                {
+                    Console.Error.WriteLine("Transfer failed: {0}", e.Message);
+                    return 1;
+                }
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-                Console.Error.WriteLine("Transfer failed: {0}",e.Message);
+                Console.Error.WriteLine("Error: {0}", e.Message);
                 return 1;
             }
         }
