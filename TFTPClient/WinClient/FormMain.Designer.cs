5f70df36d9a77e82e2888263a55a3b1948855901
jpmikkers <jean.paul.mikkers@gmail.com>
2022-03-30 17:33:50 +0200

    Use async api in winforms winclient example

--- TFTPClient/WinClient/FormMain.Designer.cs
+++ TFTPClient/WinClient/FormMain.Designer.cs
@@ -60,19 +60,19 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Location = new System.Drawing.Point(16, 22);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 17);
+            this.label1.Size = new System.Drawing.Size(50, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Server";
             // 
             // textBoxServer
             // 
-            this.textBoxServer.Location = new System.Drawing.Point(169, 15);
-            this.textBoxServer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxServer.Location = new System.Drawing.Point(169, 19);
+            this.textBoxServer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxServer.Name = "textBoxServer";
-            this.textBoxServer.Size = new System.Drawing.Size(401, 22);
+            this.textBoxServer.Size = new System.Drawing.Size(401, 27);
             this.textBoxServer.TabIndex = 1;
             this.textBoxServer.Text = "127.0.0.1:69";
             // 
@@ -80,61 +80,62 @@
             // 
             this.textBoxLocalFilename.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.textBoxLocalFilename.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
-            this.textBoxLocalFilename.Location = new System.Drawing.Point(169, 170);
-            this.textBoxLocalFilename.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxLocalFilename.Location = new System.Drawing.Point(169, 212);
+            this.textBoxLocalFilename.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxLocalFilename.Name = "textBoxLocalFilename";
-            this.textBoxLocalFilename.Size = new System.Drawing.Size(401, 22);
+            this.textBoxLocalFilename.Size = new System.Drawing.Size(401, 27);
             this.textBoxLocalFilename.TabIndex = 10;
-            this.textBoxLocalFilename.TextChanged += new System.EventHandler(this.textBoxLocalFilename_TextChanged);
+            this.textBoxLocalFilename.TextChanged += new System.EventHandler(this.TextBoxLocalFilename_TextChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 174);
+            this.label2.Location = new System.Drawing.Point(16, 218);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 17);
+            this.label2.Size = new System.Drawing.Size(106, 20);
             this.label2.TabIndex = 9;
             this.label2.Text = "Local filename";
             // 
             // textBoxRemoteFilename
             // 
-            this.textBoxRemoteFilename.Location = new System.Drawing.Point(169, 138);
-            this.textBoxRemoteFilename.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxRemoteFilename.Location = new System.Drawing.Point(169, 172);
+            this.textBoxRemoteFilename.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxRemoteFilename.Name = "textBoxRemoteFilename";
-            this.textBoxRemoteFilename.Size = new System.Drawing.Size(401, 22);
+            this.textBoxRemoteFilename.Size = new System.Drawing.Size(401, 27);
             this.textBoxRemoteFilename.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(16, 142);
+            this.label3.Location = new System.Drawing.Point(16, 178);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.Size = new System.Drawing.Size(123, 20);
             this.label3.TabIndex = 7;
             this.label3.Text = "Remote filename";
             // 
             // buttonSettings
             // 
-            this.buttonSettings.Location = new System.Drawing.Point(648, 15);
-            this.buttonSettings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonSettings.Location = new System.Drawing.Point(636, 19);
+            this.buttonSettings.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonSettings.Name = "buttonSettings";
-            this.buttonSettings.Size = new System.Drawing.Size(143, 52);
+            this.buttonSettings.Size = new System.Drawing.Size(143, 65);
             this.buttonSettings.TabIndex = 12;
             this.buttonSettings.Text = "&Settings";
             this.buttonSettings.UseVisualStyleBackColor = true;
-            this.buttonSettings.Click += new System.EventHandler(this.buttonSettings_Click);
+            this.buttonSettings.Click += new System.EventHandler(this.ButtonSettings_Click);
             // 
             // statusStrip1
             // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripProgressBar1,
             this.toolStripStatusLabel1});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 212);
+            this.statusStrip1.Location = new System.Drawing.Point(0, 260);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 19, 0);
-            this.statusStrip1.Size = new System.Drawing.Size(807, 26);
+            this.statusStrip1.Size = new System.Drawing.Size(790, 30);
             this.statusStrip1.SizingGrip = false;
             this.statusStrip1.TabIndex = 14;
             this.statusStrip1.Text = "statusStrip1";
@@ -142,13 +143,13 @@
             // toolStripProgressBar1
             // 
             this.toolStripProgressBar1.Name = "toolStripProgressBar1";
-            this.toolStripProgressBar1.Size = new System.Drawing.Size(200, 20);
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(200, 22);
             this.toolStripProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
             // 
             // toolStripStatusLabel1
             // 
             this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
-            this.toolStripStatusLabel1.Size = new System.Drawing.Size(13, 21);
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(13, 24);
             this.toolStripStatusLabel1.Text = " ";
             // 
             // comboBoxOperation
@@ -158,43 +159,43 @@
             this.comboBoxOperation.Items.AddRange(new object[] {
             "Download",
             "Upload"});
-            this.comboBoxOperation.Location = new System.Drawing.Point(169, 47);
-            this.comboBoxOperation.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboBoxOperation.Location = new System.Drawing.Point(169, 59);
+            this.comboBoxOperation.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBoxOperation.Name = "comboBoxOperation";
-            this.comboBoxOperation.Size = new System.Drawing.Size(160, 24);
+            this.comboBoxOperation.Size = new System.Drawing.Size(160, 28);
             this.comboBoxOperation.TabIndex = 3;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(16, 50);
+            this.label4.Location = new System.Drawing.Point(16, 62);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 17);
+            this.label4.Size = new System.Drawing.Size(76, 20);
             this.label4.TabIndex = 2;
             this.label4.Text = "Operation";
             // 
             // buttonStart
             // 
-            this.buttonStart.Location = new System.Drawing.Point(651, 138);
-            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.buttonStart.Location = new System.Drawing.Point(636, 94);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(143, 57);
+            this.buttonStart.Size = new System.Drawing.Size(143, 71);
             this.buttonStart.TabIndex = 13;
             this.buttonStart.Text = "&Go";
             this.buttonStart.UseVisualStyleBackColor = true;
-            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            this.buttonStart.Click += new System.EventHandler(this.ButtonStart_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(584, 167);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Location = new System.Drawing.Point(584, 209);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(41, 30);
+            this.button3.Size = new System.Drawing.Size(41, 38);
             this.button3.TabIndex = 11;
             this.button3.Text = "...";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
             // 
             // openFileDialog1
             // 
@@ -202,20 +203,20 @@
             // 
             // textBoxRemoteBaseDirectory
             // 
-            this.textBoxRemoteBaseDirectory.Location = new System.Drawing.Point(169, 106);
-            this.textBoxRemoteBaseDirectory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBoxRemoteBaseDirectory.Location = new System.Drawing.Point(169, 132);
+            this.textBoxRemoteBaseDirectory.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBoxRemoteBaseDirectory.Name = "textBoxRemoteBaseDirectory";
-            this.textBoxRemoteBaseDirectory.Size = new System.Drawing.Size(401, 22);
+            this.textBoxRemoteBaseDirectory.Size = new System.Drawing.Size(401, 27);
             this.textBoxRemoteBaseDirectory.TabIndex = 6;
-            this.textBoxRemoteBaseDirectory.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBoxRemoteBaseDirectory.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(16, 110);
+            this.label5.Location = new System.Drawing.Point(16, 138);
             this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(151, 17);
+            this.label5.Size = new System.Drawing.Size(159, 20);
             this.label5.TabIndex = 5;
             this.label5.Text = "Remote base directory";
             // 
@@ -224,14 +225,14 @@
             this.checkBoxAutoGenerateRemoteFilename.AutoSize = true;
             this.checkBoxAutoGenerateRemoteFilename.Checked = true;
             this.checkBoxAutoGenerateRemoteFilename.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.checkBoxAutoGenerateRemoteFilename.Location = new System.Drawing.Point(169, 80);
-            this.checkBoxAutoGenerateRemoteFilename.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBoxAutoGenerateRemoteFilename.Location = new System.Drawing.Point(169, 100);
+            this.checkBoxAutoGenerateRemoteFilename.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkBoxAutoGenerateRemoteFilename.Name = "checkBoxAutoGenerateRemoteFilename";
-            this.checkBoxAutoGenerateRemoteFilename.Size = new System.Drawing.Size(279, 21);
+            this.checkBoxAutoGenerateRemoteFilename.Size = new System.Drawing.Size(300, 24);
             this.checkBoxAutoGenerateRemoteFilename.TabIndex = 4;
             this.checkBoxAutoGenerateRemoteFilename.Text = "Automatically generate remote filename";
             this.checkBoxAutoGenerateRemoteFilename.UseVisualStyleBackColor = true;
-            this.checkBoxAutoGenerateRemoteFilename.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBoxAutoGenerateRemoteFilename.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
             // 
             // errorProvider1
             // 
@@ -241,7 +242,9 @@
             // 
             this.panel1.Controls.Add(this.checkBoxAutoGenerateRemoteFilename);
             this.panel1.Controls.Add(this.comboBoxOperation);
+            this.panel1.Controls.Add(this.buttonStart);
             this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.buttonSettings);
             this.panel1.Controls.Add(this.textBoxRemoteBaseDirectory);
             this.panel1.Controls.Add(this.textBoxRemoteFilename);
             this.panel1.Controls.Add(this.textBoxLocalFilename);
@@ -251,22 +254,22 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(644, 209);
+            this.panel1.Size = new System.Drawing.Size(790, 260);
             this.panel1.TabIndex = 15;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(807, 238);
+            this.ClientSize = new System.Drawing.Size(790, 290);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.buttonStart);
-            this.Controls.Add(this.buttonSettings);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "FormMain";
             this.Text = "TFTP Client";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
