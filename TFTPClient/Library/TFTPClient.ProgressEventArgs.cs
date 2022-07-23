a6f786c523e08fe58b5f0482296b7412396ac3f8
jpmikkers <jean.paul.mikkers@gmail.com>
2022-07-23 14:41:41 +0200

    Remove license duplication

---
+++ TFTPClient/Library/TFTPClient.ProgressEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GitHub.JPMikkers.TFTP.Client
+{
+
+    public partial class TFTPClient
+    {
+        public class ProgressEventArgs : EventArgs
+        {
+            public bool IsUpload { get; set; }
+            public string Filename { get; set; }
+            public long Transferred { get; set; }
+            public long TransferSize { get; set; }
+        }
+    }
+}
---
+++ TFTPClient/Library/TFTPClient.TraceEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHub.JPMikkers.TFTP.Client
+{
+
+    public partial class TFTPClient
+    {
+        public class TraceEventArgs : EventArgs
+        {
+            public string Message { get; set; }
+        }
+    }
+}
